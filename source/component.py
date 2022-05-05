25307c73600b0a44912f30f521440064ca28a104
SteBra00 <64972379+SteBra00@users.noreply.github.com>
2022-05-05 17:02:16 +0200

    Delete source directory

--- source/component.py
+++
@@ -1,9 +0,0 @@
-class Component:
-    def __init__(self, name:str, type:str, power:int, life:int=100) -> None:
-        self.name = name
-        self.type = type
-        self.power = power
-        self.life = life
-    
-    def __str__(self) -> str:
-        return f'Component: (name:{self.name}, type:{self.type}, power:{self.power}, life:{self.life})'
